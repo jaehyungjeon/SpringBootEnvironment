c7e0af35e3badc37f21260be9462c83a9d6d213a
DESKTOP-GRTABCM\JEONJAEHYUNG <jaehyung601@pionnet.co.kr>
2024-03-13 17:21:40 +0900

    password encoding not use

--- src/main/java/com/example/personal/Config/JasyptConfig.java
+++ src/main/java/com/example/personal/Config/JasyptConfig.java
@@ -4,6 +4,7 @@ import ch.qos.logback.classic.selector.servlet.LoggerContextFilter;
 import com.example.personal.Boot.URLInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
+import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
@@ -17,21 +18,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class JasyptConfig {
 
-	@Value("${}")
+    @Value("${spring.datasource.password}")
+    public String password;
 
-	@Bean(name = "jasyptStringEncryptor")
-	public StringEncryptor stringEncryptor() {
-		String key = "my_jasypt_key";
-		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(key); // 암호화할 때 사용하는 키
-		config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
-		config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
-		config.setPoolSize("1"); // 인스턴스 pool
-		config.setProviderName("SunJCE");
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
-		config.setStringOutputType("base64"); //인코딩 방식
-		encryptor.setConfig(config);
-		return encryptor;
-	}
+	/*
+	 * encrypt Hash Keyword
+	 * (Hibernate Error) -> no use
+	 **/
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        String key = "my_jasypt_key";
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key); // 암호화할 때 사용하는 키
+        config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setStringOutputType("base64"); //인코딩 방식
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 }
