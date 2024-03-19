ba5a7242feed72c97cd51a9a997445ecb8236940
JAEHYUNG <jaehyung601@naver.com>
2024-03-19 13:38:02 +0900

    [dev_20240314] 전제형 / yml Profile 분기처리(default / local)
    PersonalApplication > edit > Active Profile 값 변경하여 사용하면 됨.
    없을 시 default

--- src/main/java/com/example/personal/Service/Main/MainService.java
+++ src/main/java/com/example/personal/Service/Main/MainService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
