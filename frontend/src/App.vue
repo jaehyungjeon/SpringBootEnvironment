4c842d4ea16ba87107e143dc893522e4bbcc546e
JAEHYUNG <jaehyung601@naver.com>
2024-03-21 10:54:32 +0900

    [dev_vue_20240320] 전제형 / vue 비동기 데이터 배열 설정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,25 +2,44 @@
   <img alt="Vue logo" src="./assets/pepe.jpg">
   <HelloWorld msg="메인페이지 구성"/>
   <button v-on:click="ajaxMethod">버튼</button>
+  <button v-on:click="requestMember">ajax</button>
+  <ul>
+    <li v-for="(item, index) in array" :key="index">
+      {{ item.id }}
+    </li>
+  </ul>
 </template>
 
+<!--<script src="https://cdn.jsdelivr.net/npm/vue"></script>-->
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import axios from 'axios';
 
+let array = [{'id' : 'jaehyung!!!!'}];
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
+  data() {
+    return {
+      array : array
+    }
+  },
   methods : {
     ajaxMethod : function () {
       axios.post("/main/initMain.action")
           .then((result) => {
-            console.log(document);
+            console.log(result);
             document.querySelector("#app").innerHTML = result.data;
             console.log(result.data);
             // el.innerHTML = result.data;
+          })
+    },
+    requestMember : function() {
+      axios.get("/main/ajaxSearchGridGet.action")
+          .then((result) => {
+            this.array = result.data;
           })
     }
   }
